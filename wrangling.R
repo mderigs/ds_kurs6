f5d06ba09883843836414907d2d926e227988397
mderigs <martin@derigs.de>
2021-01-03 18:20:35 +0100

    download from url

--- wrangling.R
+++ wrangling.R
@@ -26,6 +26,7 @@ read_lines("murders.csv", n_max = 3)
 
 # read file in CSV format
 dat <- read_csv(filename)
+class(dat)
 
 #read using full path
 dat<-read_csv(fullpath)
@@ -42,3 +43,21 @@ filename2 <- "fertility-two-countries-example.csv"
 dat=read.csv(file.path(path, filename))
 dat1=read.csv(file.path(path, filename1))
 dat2=read.csv(file.path(path, filename2))
+
+
+#Standardeinlesefunktionen
+filename <- "murders.csv"
+murders=read.csv(file.path(path, filename), stringsAsFactors = TRUE)
+class(murders)
+class(murders$abb)
+class(murders$region)
+
+#Download form Internet
+url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+dat <- read_csv(url)
+download.file(url, "murders.csv")
+tempfile()
+tmp_filename <- tempfile()
+download.file(url, tmp_filename)
+dat <- read_csv(tmp_filename)
+file.remove(tmp_filename)
