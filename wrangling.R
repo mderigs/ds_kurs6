055ecc2f5d5d47acf070a360951e6192510ee376
Martin Derigs <martin@derigs.de>
2021-09-02 20:05:23 +0200

    web scraping

--- wrangling.R
+++ wrangling.R
@@ -346,3 +346,23 @@ top_names %>% semi_join(AwardsPlayers %>% filter(yearID == 2016))
 intersect(top_names %>% select(playerID), AwardsPlayers %>% filter(yearID == 2016) %>% select(playerID))
 setdiff(AwardsPlayers %>% filter(yearID == 2016) %>% select(playerID),
         top_names %>% select(playerID))
+
+
+#Web scraping
+# import a webpage into R
+library(rvest)
+url <- "https://en.wikipedia.org/wiki/Murder_in_the_United_States_by_state"
+h <- read_html(url)
+class(h)
+h
+
+tab <- h %>% html_nodes("table")
+tab <- tab[[2]]
+
+tab <- tab %>% html_table
+class(tab)
+
+tab <- tab %>% setNames(c("state", "population", "total", "murders", "gun_murders", "gun_ownership", "total_rate", "murder_rate", "gun_murder_rate"))
+head(tab)
+
+
